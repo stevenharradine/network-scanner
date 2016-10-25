d6280d691a01fbeecd493ebd86db94afa44f5c3e
Steven Harradine <stevenharradine@gmail.com>
2016-10-25 12:52:47 -0400

    pass in all arguments, network, host range, port range

--- scan.sh
+++ scan.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-range=("192.168.1."{98..99})
+network=$1
+start_address=$2
+end_address=$3
+start_port=$4
+end_port=$5
 
-for ip in "${range[@]}"
-do
-   : 
+for i in $(seq $start_address $end_address); do
+	ip=$network.$i
     echo -n "Scanning $ip "
     complete_packet_loss=`ping -c 5 $ip | grep "100% packet loss" | wc -l`
 
     if [ "$complete_packet_loss" -ne 1 ]; then	# if you can ping the box then
-    	./port-scanner.sh $ip 1 65535 | tee ports.txt
+    	./port-scanner.sh $ip $start_port $end_port | tee ports.txt
 		ports=`cat ports.txt`
 
 		webpage_requiring_authentication=`echo $ports | grep :80 | wc -l`
